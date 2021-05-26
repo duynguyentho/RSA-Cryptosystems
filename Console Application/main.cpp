1657fcbb08aacb92fe74809cd1acffe562436e86
duynt2201 <duynt2201@gmail.com>
2021-05-26 14:51:52 +0700

    final

--- Console Application/main.cpp
+++ Console Application/main.cpp
@@ -12,42 +12,42 @@
 #include <sstream>
 #include <stdio.h>
 //use boost library
-//#include <boost/archive/iterators/base64_from_binary.hpp>
-//#include <boost/archive/iterators/insert_linebreaks.hpp>
-//#include <boost/archive/iterators/transform_width.hpp>
-//#include <boost/archive/iterators/ostream_iterator.hpp>
-//#include <boost/algorithm/string/std/list_traits.hpp>
-//void convertToBase64(std::string a){
-//    using namespace boost::archive::iterators;
-//    std::string test = a;
-//    std::stringstream os;
-//        typedef
-//            insert_linebreaks<
-//                base64_from_binary<
-//                    transform_width<
-//                        const char *,
-//                        6,
-//                        8
-//                    >
-//                >
-//                ,72
-//            >
-//            base64_text;
-//
-//        std::copy(
-//            base64_text(test.c_str()),
-//            base64_text(test.c_str() + test.size()),
-//            ostream_iterator<char>(os)
-//        );
-//
-//        std::cout << os.str();
-//}
+#include <boost/archive/iterators/base64_from_binary.hpp>
+#include <boost/archive/iterators/insert_linebreaks.hpp>
+#include <boost/archive/iterators/transform_width.hpp>
+#include <boost/archive/iterators/ostream_iterator.hpp>
+#include <boost/algorithm/string/std/list_traits.hpp>
+void convertToBase64(std::string a){
+    using namespace boost::archive::iterators;
+    std::string test = a;
+    std::stringstream os;
+        typedef
+            insert_linebreaks<
+                base64_from_binary<
+                    transform_width<
+                        const char *,
+                        6,
+                        8
+                    >
+                >
+                ,72
+            >
+            base64_text;
+
+        std::copy(
+            base64_text(test.c_str()),
+            base64_text(test.c_str() + test.size()),
+            ostream_iterator<char>(os)
+        );
+
+        std::cout << os.str();
+}
 using namespace std;
 long int p, q, n, phi_n, e, d, b, s;
 string inputMsg; // chuoi ki tu nhap vao
-char* eMsg[500]; // encrypt Message
+char* eMsg; // encrypt Message
 int temp[100];
-char dMsg; // decrypt Message
+char* dMsg; // decrypt Message
 long int extendEuclide(long int, long int);
 void convertStringToInt(char c[]);
 bool isPrime(long int n);
@@ -83,33 +83,21 @@ long int getB(long int y){
     return 0;
 }
 long int extendEuclide(long int b, long int a){
-    long int result, y0=0, y1=1, r, q;
-    while(a>0)
-        {
-            r=b%a;
-            if(r==0) break;
-            else
-            {
-            q=b/a;
-            result=y0-y1*q;
-            b=a;
-            a=r;
-            y0=y1;
-            y1=result;
-            }
-        }
-    if(result < 0){
-        return result + phi_n;
-    }
-    return result;
+    for (long int l = 1; l < b; l++)
+                {
+                    if ((a * l) % b == 1)
+                        return l;
+                }
+                return -1;
 }
 long int modulo(long int a, long int b, long int n){
-    long long x = 1, y = a;
-    while(b > 0){
-        x = x * y;
-        --b;
+    long result = 1L;
+    for (long l = 0; l < b; l++)
+    {
+        result = (result * a) % n;
     }
-    return x % n;
+    return result;
+    
 }
 char* vh(string str){
     char* c = new char[str.size() + 1];
@@ -122,9 +110,9 @@ void convertStringToInt(char c[]){
     for(int i = 0; i< length; i++){
         temp[i] = c[i] - char(65);
     }
-    for(int i = 0;  i < length ; i++){
-        cout << temp[i]<<endl;
-    }
+//    for(int i = 0;  i < length ; i++){
+//        cout << temp[i]<<endl;
+//    }
 }
 char* encrypt(string s){
     char* x = new char(s.size() + 1);
@@ -135,64 +123,86 @@ char* encrypt(string s){
         temp[i] = modulo(temp[i], b, n);
         x[i] = convertIntToChar(temp[i]);
     }
-    for(int i = 0;  i < length ; i++){
-        cout <<x[i]<<"\t"<< temp[i]<<endl;
-    }
+//    for(int i = 0;  i < length ; i++){
+//        cout << temp[i]<<endl;
+//    }
     return x;
 }
 char* decrypt(string str){
     char* x = new char(str.size() + 1);
     char* mau = vh(str);
-    convertStringToInt(mau);
     int length = strlen(mau);
+    cout <<"\n------------------------------\n";
     for(int i = 0; i< length; i++){
         temp[i] = modulo(temp[i], s, n);
         x[i] = convertIntToChar(temp[i]);
     }
-        for(int i = 0;  i < length ; i++){
-            cout << temp[i]<<endl;
-        }
+//        for(int i = 0;  i < length ; i++){
+//            cout << temp[i]<<endl;
+//        }
     return x;
+}
+void getPair(){
+    srand((int)time(0));
+    do{
+        p = random(3, 3000);
+    }while(!isPrime(p));
+    do{
+        q = random(3, 3000);
+    }while(!isPrime(q) && p != q);
 }
 void Running(){
     long int x1, y1;
-    cout << "Input p: \n";
-    cin >> p;
-    cout <<"Input q: \n";
-    cin >> q;
-    if(!isPrime(p) || !isPrime(q)){
-        cout <<"\nInput a pair of prime number !";
-        return ;
-    }
-    if(p == q){
-        cout << "p can't equal q";
-        return;
-    }
+//    cout << "Input p: \n";
+//    cin >> p;
+//    cout <<"Input q: \n";
+//    cin >> q;
+//    if(!isPrime(p) || !isPrime(q)){
+//        cout <<"\nInput a pair of prime number !";
+//        return ;
+//    }
+//    if(p == q){
+//        cout << "p can't equal q";
+//        return;
+//    }
+    getPair();
     n = p * q;
     phi_n = (p - 1)*(q - 1);
     b = getB(phi_n);
-    cout << "Input message: \n";
-    cin >>inputMsg;
     cout<<"-----------------\n";
     cout << "p: "<<p;
     cout <<"\nq: "<<q;
-    cout << "\n n: "<< n;
+    cout << "\nn: "<< n;
     cout <<"\ny: "<<phi_n;
     cout <<"\nb: "<<b;
-    cout <<"\nKpublic(n, b): ("<<n<<", "<<b<<")";
+    cout <<"\nKpublic(n, b): ("<<n<<", "<<b<<")"<<endl;
+    std::ostringstream kpub;
+    kpub << n << b;
+    const std::string kpu(kpub.str());
+    convertToBase64(kpu);
     s = extendEuclide(phi_n, b);
     cout <<"\nKprivate: "<< s <<endl;
-    cout <<"\n Encrypt: ";
-    cout <<encrypt(inputMsg)<< endl;
-    char* v = encrypt(inputMsg);
-    cout << decrypt(v);
+    std::ostringstream kpriv;
+    kpriv << s << n;
+    const std::string kpr(kpriv.str());
+    convertToBase64(kpr);
+    cout << "\nInput message: \n";
+    cin >>inputMsg;
+    std::string input(inputMsg);
+    convertToBase64(input);
+    cout <<"\nEncrypt: \n---------------------------------\n" <<encrypt(inputMsg)<< endl;
+    dMsg = encrypt(inputMsg);
+    cout <<"\nDecrypt: " <<decrypt(dMsg)<<endl;
+//    std::string str(decrypt(dMsg));
+//    convertToBase64(str);
     return;
 }
 char convertIntToChar(int i){
     return char(65+i);
 }
 int main(int argc, const char * argv[]) {
-    Running();
+   Running();
     //cout << char(65);
+    //getPair();
     return 0;
 }
--- README.md
+++ README.md
@@ -5,4 +5,10 @@ using Qt C++ and boost library
 Created by wuyxz on 24/05/2022
 
 Required Qt 5.15 and boost Library
+ 
+ Open console Application folder to run console App 
+ 
+ Open  Qt project to run Qt Application
+ 
+ 
 
